e0deac6df2b27e6c0642fd877cd7d2e2ff83c8fe
Branden McCarthy <bmccarthy2@ucmerced.edu>
2022-05-11 16:19:09 -0700

    Improved `extractMeasurement` function and added support for inputs containing '&' or 'and' between a whole number and fraction(e.g. '1 & 1/2 cup'). Also, improved `extractName` function to seperate ingredient name and additional info.

--- index.js
+++ index.js
@@ -15,9 +15,29 @@ const { toNumberFromString, convertUnicodeFractions } = require('./src/utils');
             isRange:    Boolean
         }
     The second entry is the input ingredient string with the measurement information removed
+
+    The algorithm is as follows:
+        1.  Pre-process string
+        2.  Extract Quantity portion of string, if present
+        3.  Extract Unit portion of string, if present
+            3.1.  If package-like unit is found(eg. package, bag, box, etc.):
+                3.1.1.  Remove the unit.
+            3.2.  If regular unit is found(eg. liter, ounce, cup, etc.):
+                3.2.1.  If unit is found at the beginning of the string: remove the unit.
+                3.2.2.  If unit is not at the beginning of the string: assume that in front of the unit is an adjective describing
+                        the unit (ex. 'heaping tablespoons of coffee grinds' or 'level cup of flour') *Quantity is removed at this point*.
+                        Remove the unit and the portion of the string that was in front of the unit.
+        4.  Extract Converted Quantity/Unit of string, if present
+            4.1. Follow steps '2.' and '3.2.'(assume no package-like unit)
+        5.  Save the results from steps '2.', '3.' as result.measurement and the results of step '4.' as result.convertedMeasurement.
+        6.  Seperate the ingredient name from the rest of the ingredient string. Assume the ingredient name will be at the beginning of the
+            string and determine the separation point as the location where either the first comma or the first opening parenthesis is found.
+            6.1 Examine the name section of the string(first section) for anything which is not part of the ingredient name.
+                (ex. 'finely chopped cabbage, any variety' or 'very ripe bananas, mashed'). Move this part to the end of the second section.
+            6.2. Save the first section as result.name and the second section as result.additional
 */
 function extractMeasurement(ingredientString) {
-    const measurements = [];
+    const measurements = [null, null];
     const measurement = {
         quantity: null,
         unit: null,
@@ -36,19 +56,24 @@ function extractMeasurement(ingredientString) {
     ingredientString = ingredientString.replace(/(?<!\d)\//g, ' /');
     //If there is an opening parens immediately proceeded by a digit and immediately preceeded by a non-digit,
     //assume it is a measurement conversion and add space. Ex: '28g(1oz) flour' becomes '28g (1oz) flour'.
-    ingredientString = ingredientString.replace(/(?<!\d)\((?=\d)/g, ' (');
+    ingredientString = ingredientString.replace(/(?<!\d|\s)\((?=\d)/g, ' (');
+    //If 'and' or '&' is immediately preceeded by a whole number and immediately proceeded by a fraction, 
+    //remove the 'and' or '&'. Ex: '1 & 1/2 cups' becomes '1 1/2 cups'
+    ingredientString = ingredientString.replace(/(?<=\d+)(\s*(and|&)\s*)(?=\d+\/\d+)/g, " ");
     //If there is a slash contained between two digits, assume it is a fraction and convert to a decimal
     ingredientString = ingredientString.replace(/(\d*)\s*(\d+\/\d+)/g, (match, whole, frac) => (toNumberFromString(whole) + toNumberFromString(frac)));
+    //If the string begins with the word 'A' or 'An', assume it to mean a single unit of ingredient and replace with the number '1'
+    ingredientString = ingredientString.replace(/^an?\s+/i, "1 ");
 
     //Extract the measurement quantity. Assumes measurement is at start of string.
     //If quantity is a range, assumes it to be structured like: '1-2' or '1 to 2' (may include whitespace in between)
-    const quantityRegex = /^(\d+\.?\d*)\s*(\-|to)*\s*(\d+\.?\d*)*\s*/;
+    const quantityRegex = /^(\d+\.?\d*)\s*(\-|to(?=\d+|\s+))*\s*(\d+\.?\d*)*\s*/;
     //Three groups are captured:
     //  1. First number/decimal
     //  2. Range identifier, '-' or 'to' (if exists)
     //  3. Second number/decimal (if exists)
     let quantityStringLength = 0; //Length of the quantity portion of the string
-    const quantityMatch = ingredientString.replace(/^an?\s+/i, "1 ").match(quantityRegex);
+    const quantityMatch = ingredientString.match(quantityRegex);
 
     if(quantityMatch) {
         const minQuantity = toNumberFromString(quantityMatch[1]);
@@ -67,13 +92,12 @@ function extractMeasurement(ingredientString) {
     let ingredientStringWithoutQuantityAndUnit = ingredientStringWithoutQuantity;
     
     //Extract the measurement unit
-    //If the unit is 'bag', 'box', 'can', or 'package', it may come with a second measurement that gives the size
-    //of the package(Ex: '1 (8 oz.) can of tomato paste' or '2 cans of tomato paste (8oz)'. Check this first and move second
-    //measurement to the end of the string.
+
+    //First check if the unit is 'bag', 'box', 'can', or 'package'
     const packageRegex = /\b(bags?|boxe?s?|cans?|packages?|pkgs?\.?)\b/i
     const packageMatch = ingredientStringWithoutQuantity.match(packageRegex);
     if(packageMatch) {
-        ingredientStringWithoutQuantityAndUnit = ingredientStringWithoutQuantityAndUnit.slice(packageMatch.index + packageMatch[0].length).trim() + " " + ingredientStringWithoutQuantityAndUnit.slice(0, packageMatch.index).trim();
+        ingredientStringWithoutQuantityAndUnit = ingredientStringWithoutQuantityAndUnit.slice(0, packageMatch.index).trim() + ingredientStringWithoutQuantityAndUnit.slice(packageMatch.index + packageMatch[0].length).trim();
         measurement.unit = unitMap.get(packageMatch[0].replace(".", "").toLowerCase().trim());
     }
 
@@ -83,27 +107,39 @@ function extractMeasurement(ingredientString) {
     const unitRegex = /\b(cups?|c\.?|cloves?|gallons?|gals?\.?|ounces?|oz\.?|pints?|pts?\.?|pounds?|lbs?\.?|quarts?|qts?\.?|tablespoons?|tbsp?n?s?\.?|teaspoons?|tspn?s?\.?|grams?|g\.?|kilograms?|kgs?\.?|liters?|lt?\.?|milligrams?|mgs?\.?|milliliters?|mls?\.?|pieces?|pcs?\.?|pinche?s?|slices?|sticks?|small|sm\.?|medium|med\.?|large|lg\.?)(?!\S)/i
     //One group is captured: the unit (without ending period, if any)
     const unitMatch = ingredientStringWithoutQuantityAndUnit.match(unitRegex);
-    const conversionRegex = /^(\(|\/)\s*(\d+\.?\d*)\s*(cups?|c\.?|gallons?|gals?\.?|ounces?|oz\.?|pints?|pts?\.?|pounds?|lbs?\.?|quarts?|qts?\.?|tablespoons?|tbsp?n?s?\.?|teaspoons?|tspn?s?\.?|grams?|g\.?|kilograms?|kgs?\.?|liters?|lt?\.?|milligrams?|mgs?\.?|milliliters?|mls?\.?|pieces?|pcs?\.?|pinche?s?|pieces?|pcs?\.?|slices?|sticks?)(\)?)/;
-    let conversionMatch, conversionStringLength = 0;
     if(!packageMatch && unitMatch) {
         unitStringLength = unitMatch[0].length;
         unitStringStart = unitMatch.index;
-
+        
         measurement.unit = unitMap.get(unitMatch[1].replace(".", "").toLowerCase().trim());
         ingredientStringWithoutQuantityAndUnit = ingredientStringWithoutQuantityAndUnit.slice(0, unitStringStart).trim() + (unitStringStart > 0 ? " " : "") + ingredientStringWithoutQuantityAndUnit.substring(unitStringStart + unitStringLength).trim();
+    }
 
-        //Decide if there is a measurement conversion next. Ex: '28 grams/1oz sugar' or '28 g (1 oz.) sugar'
-        conversionMatch = ingredientStringWithoutQuantityAndUnit.match(conversionRegex);
+    //If string begins with preposition 'of', remove it
+    ingredientStringWithoutQuantityAndUnit = ingredientStringWithoutQuantityAndUnit.replace(/^of\s/, "");
 
-        if(conversionMatch) {
-            conversionStringLength = conversionMatch[0].length;
-            conversion.quantity = toNumberFromString(conversionMatch[2]);
-            conversion.unit = unitMap.get(conversionMatch[3].replace(".", "").toLowerCase().trim());
+    const conversionRegex = /^(\(|\/)\s*(\d+\.?\d*)\s*(\-|to)*\s*(\d+\.?\d*)*\s*(cups?|c\.?|gallons?|gals?\.?|ounces?|oz\.?|pints?|pts?\.?|pounds?|lbs?\.?|quarts?|qts?\.?|tablespoons?|tbsp?n?s?\.?|teaspoons?|tspn?s?\.?|grams?|g\.?|kilograms?|kgs?\.?|liters?|lt?\.?|milligrams?|mgs?\.?|milliliters?|mls?\.?|pieces?|pcs?\.?|pinche?s?|pieces?|pcs?\.?|slices?|sticks?)(\)?)/;
+    let conversionMatch, conversionStringLength = 0;
+    //Decide if there is a measurement conversion next. Ex: '28 grams /1oz sugar' or '28 g (1 oz.) sugar'
+    conversionMatch = ingredientStringWithoutQuantityAndUnit.match(conversionRegex);
 
-            ingredientStringWithoutQuantityAndUnit = ingredientStringWithoutQuantityAndUnit.substring(conversionStringLength).trim();
+    if(conversionMatch) {
+        const minQuantity = toNumberFromString(conversionMatch[2]);
+        const isRange = conversionMatch[3];
+        const maxQuantity = conversionMatch[4] ? toNumberFromString(conversionMatch[4]) : minQuantity;
+        //Length of converted measurement quantity portion of string
+        conversionStringLength = conversionMatch[0].length;
+        
+        if(isRange) {
+            conversion.isRange = true;
+            conversion.quantity = [minQuantity, maxQuantity];
+        } else conversion.quantity = minQuantity;
 
-            measurements[1] = conversion;
-        }
+        conversion.unit = unitMap.get(conversionMatch[5].replace(".", "").toLowerCase().trim());
+
+        ingredientStringWithoutQuantityAndUnit = ingredientStringWithoutQuantityAndUnit.substring(conversionStringLength).trim();
+
+        measurements[1] = conversion;
     }
 
     measurements[0] = measurement;
@@ -111,12 +147,14 @@ function extractMeasurement(ingredientString) {
     return [measurements, ingredientStringWithoutQuantityAndUnit]
 }
 
-//TODO: Seperate ingredient name from the rest of the ingredient string, if any.
-//Ex: extractName('garlic') returns ['garlic', null]
-//Ex: extractName('garlic, minced') returns ['garlic', 'minced']
-//Ex: extractName('garlic (peeled and chopped)') returns ['garlic', '(peeled and chopped)']
+//TODO: Account for additional info which is before the ingredient name (ex: '1 finely chopped onion')
+//Assumes the input begins with the ingredient name and any additional text will be after a ',' or within '(' and ')'.
 function extractName(ingredientString) {
-    return [ingredientString.trim().replace(/^of\s/, ""), null];
+    ingredientString = ingredientString.replace(/^of\s/, "").trim();
+    const additionalRegex = /s*(,|\()/;
+    const additionalMatch = ingredientString.match(additionalRegex);
+    if(additionalMatch) return [ingredientString.substring(0, additionalMatch.index).trim(), ingredientString.substring(additionalMatch.index + 1).replace(/\)$/, "").trim()];
+    return [ingredientString, null];
 }
 
 function parse(ingredientString) {
